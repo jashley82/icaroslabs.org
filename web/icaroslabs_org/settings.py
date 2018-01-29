6d502009bfca514fcb41f4b4f6683fa294543447
josh <ashley.joshua@gmail.com>
2018-01-29 15:38:10 -0800

    add domain

--- web/icaroslabs_org/settings.py
+++ web/icaroslabs_org/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'fe2d!*=yethkug0-sbl=k6dh$_7qyk6j41^$uf705cy(!%@7x3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.icaroslabs.org']
 
 
 # Application definition
