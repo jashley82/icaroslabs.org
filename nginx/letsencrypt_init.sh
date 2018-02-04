142403d56ffc6f8cd78d6b75c46753a739cf748a
josh <ashley.joshua@gmail.com>
2018-02-03 16:19:11 -0800

    letsencrypt host init

--- nginx/letsencrypt_init.sh
+++ nginx/letsencrypt_init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 
+    exit 1
+fi
+
 docker-compose exec nginx /usr/bin/ceretbot-auto certonly \
     -n --nginx --agree-tos --email icaroslabs@gmail.com -d icaroslabs.org
 
-sudo crontab crontab.txt
+crontab letsencrypt/crontab.txt
